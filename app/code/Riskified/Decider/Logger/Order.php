d12dba3f7ae7ae32000f97ca8383ecc2dc28c80b
Nachshon Fishbane <nahshf@gmail.com>
2017-01-10 12:24:12 +0100

    Refactored logger class to use contruct method

--- app/code/Riskified/Decider/Logger/Order.php
+++ app/code/Riskified/Decider/Logger/Order.php
@@ -3,4 +3,11 @@ namespace Riskified\Decider\Logger;
 
 class Order extends \Monolog\Logger
 {
+    /**
+     * @inheritdoc
+     */
+    public function __construct($name, array $handlers = array(), array $processors = array())
+    {
+        parent::__construct($name, $handlers, $processors);
+    }
 }
