9d60231028f20b7d4f6a7c34b525ed52e936752e
Wojciech Kamiski <wojtek.kaminski@gmail.com>
2019-04-10 07:59:00 +0200

    Removed including riskified sdk module in registration pph

--- registration.php
+++ registration.php
@@ -1,5 +1,4 @@
 <?php
-include_once BP . '/vendor/riskified/php_sdk/src/Riskified/autoloader.php';
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Riskified_Decider',
