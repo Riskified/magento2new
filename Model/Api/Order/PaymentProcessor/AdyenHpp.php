c24a6b45817f93fafe453f4f83bd951889274c15
Rich Spitkovsky <44982954+rich-spitkovsky-riskified@users.noreply.github.com>
2021-01-21 10:39:12 -0500

    fix typo in payer_status

--- Model/Api/Order/PaymentProcessor/AdyenHpp.php
+++ Model/Api/Order/PaymentProcessor/AdyenHpp.php
@@ -13,7 +13,7 @@ class AdyenHpp extends AbstractPayment
 
         if (strtolower($this->payment->getCcType()) == "paypal") {
             $details['payer_email'] = $this->payment->getAdditionalInformation('paypal_payer_email');
-            $details['payer_statis'] = $this->payment->getAdditionalInformation('paypal_payer_status');
+            $details['payer_status'] = $this->payment->getAdditionalInformation('paypal_payer_status');
             $details['payer_address_status'] = $this->payment->getAdditionalInformation('paypal_address_status');
             $details['protection_eligibility'] = $this->payment->getAdditionalInformation('paypal_protection_eligibility');
             $details['payment_status'] = $this->payment->getAdditionalInformation('paypal_payment_status');
