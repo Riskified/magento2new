cb098528b389f0a126f5e324cd2f6a59cabd41fb
Wojtek Kaminski <wojtek.kaminski@gmail.com>
2021-05-28 08:08:20 +0200

    Added deco payment base in riskified extension

---
+++ view/frontend/web/js/payment.js
@@ -0,0 +1,91 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'ko',
+    'jquery',
+    'uiComponent',
+    'Magento_Checkout/js/action/place-order',
+    'Magento_Checkout/js/action/select-payment-method',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Customer/js/model/customer',
+    'Magento_Checkout/js/model/payment-service',
+    'Magento_Checkout/js/checkout-data',
+    'Magento_Checkout/js/model/checkout-data-resolver',
+    'uiRegistry',
+    'Magento_Checkout/js/model/payment/additional-validators',
+    'Magento_Ui/js/model/messages',
+    'uiLayout',
+    'Magento_Checkout/js/action/redirect-on-success'
+], function (
+    ko,
+    $,
+    Component,
+    placeOrderAction,
+    selectPaymentMethodAction,
+    quote,
+    customer,
+    paymentService,
+    checkoutData,
+    checkoutDataResolver,
+    registry,
+    additionalValidators,
+    Messages,
+    layout,
+    redirectOnSuccessAction
+) {
+    'use strict';
+
+    var mixin = {
+
+        /**
+         * Place order.
+         */
+        placeOrder: function (data, event) {
+            var self = this;
+            if (event) {
+                event.preventDefault();
+            }
+            if (this.validate() && additionalValidators.validate()) {
+                this.isPlaceOrderActionAllowed(false);
+
+                this.getPlaceOrderDeferredObject()
+                    .fail(
+                        function () {
+                            self.triggerCheckoutDeniedCall();
+                            $(document).trigger('paymentFail');
+                            self.isPlaceOrderActionAllowed(true);
+                        }
+                    ).done(
+                    function () {
+                        self.afterPlaceOrder();
+
+                        if (self.redirectAfterPlaceOrder) {
+                            redirectOnSuccessAction.execute();
+                        }
+                    }
+                );
+
+                return true;
+            }
+
+            return false;
+        },
+        triggerCheckoutDeniedCall: function()
+        {
+            storage.post(
+                'decider/checkout/checkoutDenied',
+                JSON.stringify({
+                    quote_id: quote.getQuoteId()
+                }),
+                true
+            );
+        }
+    };
+
+    return function (target) {
+        return target.extend(mixin);
+    };
+});
