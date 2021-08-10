1fd91306375f22557ef9235299253c3ff01b5657
arungopalRF <36234947+arungopalRF@users.noreply.github.com>
2021-08-10 16:08:30 -0400

    Revert "TIS-316:SCA additional payment data + Checkout denied"

--- view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Checkout/js/view/payment/default': {
-                'Riskified_Decider/js/payment': true
-            },
-        }
-    }
-};
