d6901948708db87f22a663576cf7226c411ee71b
Wojtek <wojtek.kaminski@gmail.com>
2023-01-03 17:17:58 +0100

    Added processed order to magento registry to prevent bubbling events

--- Model/Observer/SalesOrderShipmentSaveAfter.php
+++ Model/Observer/SalesOrderShipmentSaveAfter.php
@@ -4,6 +4,7 @@ namespace Riskified\Decider\Model\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Registry;
 use Riskified\Decider\Model\Api\Api;
 use Riskified\Decider\Model\Api\Log as LogApi;
 use Riskified\Decider\Model\Api\Order as OrderApi;
@@ -26,17 +27,23 @@ class SalesOrderShipmentSaveAfter implements ObserverInterface
     private OrderApi $apiOrderLayer;
 
     /**
+     * @var Registry
+     */
+    protected Registry $registry;
+
+    /**
      * SalesOrderShipmentSaveAfter constructor.
      *
      * @param Api $api
      * @param LogApi $logger
      * @param OrderApi $orderApi
      */
-    public function __construct(Api $api, LogApi $logger, OrderApi $orderApi)
+    public function __construct(Api $api, LogApi $logger, OrderApi $orderApi, Registry $registry)
     {
         $this->api = $api;
         $this->logger = $logger;
         $this->apiOrderLayer = $orderApi;
+        $this->registry = $registry;
     }
 
     /**
@@ -49,6 +56,8 @@ class SalesOrderShipmentSaveAfter implements ObserverInterface
         $this->api->initSdk($shipment->getOrder());
 
         try {
+            $this->registry->register("riskified-order", $shipment->getOrder(), true);
+
             $this->apiOrderLayer->post($shipment, Api::ACTION_FULFILL);
         } catch (\Exception $e) {
             $this->logger->log(
