0ef9d366442e533433cf59851c35cce89edcd536
Wojtek <wojtek.kaminski@gmail.com>
2023-04-14 10:35:18 +0200

    Reverted place after

--- Model/Observer/OrderPlacedAfter.php
+++ Model/Observer/OrderPlacedAfter.php
@@ -54,7 +54,7 @@ class OrderPlacedAfter implements ObserverInterface
             return;
         }
 
-        if ($order->getPayment()->getMethod() == "flxpayment" || $order->getPayment()->getMethod() == "adyen_cc") {
+        if ($order->getPayment()->getMethod() == "flxpayment") {
             return;
         }
 
